3bd0c0d9756e847d07a67d1ce5a36929067fd36e
liu <nian.liu@keepsoft.net>
2020-07-09 14:14:00 +0800

    独立pickerOptions

--- web/src/components/QueryBills.vue
+++ web/src/components/QueryBills.vue
@@ -96,6 +96,7 @@
 
 <script>
 import api from "@/api";
+import { pickerOptions } from "@/lib/element";
 import NumberPlate from "@/components/NumberPlate";
 
 export default {
@@ -142,43 +143,7 @@ export default {
           value: false
         }
       ],
-      pickerOptions: {
-        shortcuts: [
-          {
-            text: "最近一周",
-            start: new Date(new Date().getTime() - 3600 * 1000 * 24 * 7)
-          },
-          {
-            text: "最近一个月",
-            start: new Date(new Date().getTime() - 3600 * 1000 * 24 * 30)
-          },
-          {
-            text: "最近三个月",
-            start: new Date(new Date().getTime() - 3600 * 1000 * 24 * 90)
-          },
-          {
-            text: "最近六个月",
-            start: new Date(new Date().getTime() - 3600 * 1000 * 24 * 180)
-          },
-          {
-            text: "最近一年",
-            start: new Date(new Date().getTime() - 3600 * 1000 * 24 * 365)
-          },
-          {
-            text: "一年以上",
-            start: new Date(1970, 1, 1),
-            end: new Date(new Date().getTime() - 3600 * 1000 * 24 * 365)
-          }
-        ].map(item => ({
-          text: item.text,
-          onClick(picker) {
-            picker.$emit("pick", [
-              item.start,
-              item["end"] ? item["end"] : new Date()
-            ]);
-          }
-        }))
-      },
+      pickerOptions,
       form: {
         finished: "",
         createdAtInterval: [],
