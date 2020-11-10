73a7d73ec8e8b158d754314168aabe55964701a8
liu <nian.liu@keepsoft.net>
2020-11-10 15:13:38 +0800

    添加项目启动时会初始化必要数据

--- server/db/index.go
+++ server/db/index.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"myModule/lib"
 	"myModule/types"
-	"time"
 )
 
 //数据库文件
@@ -19,43 +17,13 @@ func InitDB(config types.DbConfig) {
 	if err != nil {
 		fmt.Println("数据库连接失败: ", err)
 	}
-	_ = DB.AutoMigrate(&types.User{})
-	_ = DB.AutoMigrate(&types.ShopInfo{})
+
 	initDBData(DB)
 }
+
 func initDBData(db *gorm.DB) {
 	//需要初始化的数据库数据
-	initShopInfo(db)
-}
-
-func initShopInfo(db *gorm.DB) {
-	//没有门店信息的时候新建一条
-	var shopInfo types.ShopInfo
-	result := db.First(&shopInfo)
-	if result.Error != nil {
-		info := types.ShopInfo{Name: "欣念", Introduction: "说明", Suffix: "智慧门店"}
-		_ = db.Create(&info)
-	}
-
-	var userInfo types.User
-	result = db.First(&userInfo)
-	if result.Error != nil {
-		newUser := types.User{
-			Username: "admin",
-			Password: "Aa123456",
-			Phone:    "",
-			Email:    "",
-			Role:     1,
-			Birthday: time.Now(),
-		}
-		salt, pas := lib.EncryptionString(newUser.Password)
-		newUser.Password = pas
-		newUser.Salt = salt
-		res := DB.Create(&newUser)
-		if res.Error != nil {
-			fmt.Println("新建默认账户数据库写入错误！")
-		} else {
-			fmt.Println("新建默认账户成功！")
-		}
-	}
+	InitAdminUser(db)
+	InitShopInfo(db)
+	InitDashboardMenus(db)
 }
---
+++ server/db/initDBData.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"myModule/lib"
+	"myModule/types"
+	"time"
+)
+
+func InitAdminUser(db *gorm.DB) {
+	//初始化管理员账户
+	_ = db.AutoMigrate(&types.User{})
+	var userInfo types.User
+	result := db.First(&userInfo)
+	if result.Error != nil {
+		newUser := types.User{
+			Username: "admin",
+			Password: "Aa123456",
+			Phone:    "",
+			Email:    "",
+			Role:     1,
+			Birthday: time.Now(),
+		}
+		salt, pas := lib.EncryptionString(newUser.Password)
+		newUser.Password = pas
+		newUser.Salt = salt
+		res := DB.Create(&newUser)
+		if res.Error != nil {
+			fmt.Println("新建默认账户数据库写入错误！")
+		} else {
+			fmt.Println("新建默认账户成功！")
+		}
+	}
+}
+
+func InitShopInfo(db *gorm.DB) {
+	//初始化店铺信息
+	_ = db.AutoMigrate(&types.ShopInfo{})
+	var shopInfo types.ShopInfo
+	result := db.First(&shopInfo)
+	if result.Error != nil {
+		info := types.ShopInfo{Name: "欣念", Introduction: "说明", Suffix: "智慧门店"}
+		_ = db.Create(&info)
+	}
+}
+
+func InitDashboardMenus(db *gorm.DB) {
+	//初始化菜单
+	_ = db.AutoMigrate(&types.DashboardMenu{})
+	var dashboardMenu types.DashboardMenu
+	result := db.First(&dashboardMenu)
+	if result.Error != nil {
+		defaultMenus := []types.DashboardMenu{
+			{Title: "资产分析", Path: "/dashboard/analysis", Icon: "LineChartOutlined"},
+			{Title: "订单管理", Path: "/dashboard/order-management", Icon: "FileTextOutlined"},
+			{Title: "系统设置", Path: "/dashboard/settings", Icon: "SettingOutlined"}}
+		for _, m := range defaultMenus {
+			menu := types.DashboardMenu{Title: m.Title, Path: m.Path, Icon: m.Icon}
+			_ = DB.Create(&menu)
+		}
+	}
+}
