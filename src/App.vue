86f394834efc5185eba318a30f58fd048d6de41b
liu <liunian@2077tech.com>
2022-07-06 19:44:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import { useTitle } from "@vueuse/core";
+
+useTitle(import.meta.env.VITE_APP_TITLE);
+</script>
+
+<template>
+  <header>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <el-button>I am ElButton</el-button>
+    <button class="btn">Button</button>
+  </header>
+
+  <RouterView />
+</template>
